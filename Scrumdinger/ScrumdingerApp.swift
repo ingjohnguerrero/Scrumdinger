f6ecbe6b10590ffc5f26f226d71fcbca96ca90c1
John Guerrero <ingjohnguerrero@gmail.com>
2025-08-28 12:30:03 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by John Guerrero on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
